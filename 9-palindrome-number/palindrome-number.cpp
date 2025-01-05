1584991099e882ee538d5c5499eb03422abf0b06
KARAN <123332277+karan79k@users.noreply.github.com>
2025-01-05 16:16:46 +0530

    Update palindrome-number.cpp

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//<h2>for solution </h2>
+//just reverse the number and compare to the original
     bool isPalindrome(int x) {
         if(x < 0)return false;
         if(x==0)return true;
